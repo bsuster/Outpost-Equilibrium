09767fef29b9d34c91833b1219a620e351bd4501
Benjamin <43838795+bsuster@users.noreply.github.com>
2025-04-17 02:10:48 -0400

    Added more fleshed out commands

--- scenes/characters/terminal.gd
+++ scenes/characters/terminal.gd
@@ -9,10 +9,12 @@ signal printing_done
 @onready var type_audio_player: AudioStreamPlayer2D = $AudioPlayer_Typing
 
 var is_printing: bool = false
+var previous_command: String = ""
 
 func _ready():
 	terminal_input.set_keep_editing_on_text_submit(true)
-	#enable_input()
+	if get_tree().current_scene == self:
+		enable_input()
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
@@ -30,24 +32,38 @@ func submit_input():
 	if is_printing:
 		return
 	var command = terminal_input.text
+	var regex = RegEx.new()
+	regex.compile("\\[.*?\\]")
+	command = regex.sub(command, "", true).to_lower()
 	terminal_input.clear()
 	
 	var command_arr = command.split(" ")
 	var command_text = "> " + command
+	print_terminal_output(command_text)
+	await printing_done
 	if not CommandManager.has_command(command):
 		return
 	
 	var command_output = CommandManager.exec_command(command_arr[0])
 	
-	print_terminal_output( "\n\n" + command_text + "\n\n" + command_output)
+	print_terminal_output(command_output)
 
 func print_terminal_output(output: String) -> void:
 	is_printing = true
+	var typing_pitch_range = range(7, 8, 1)
 	var characters = output.split()
 	for character in characters:
-		terminal_output.text += character
-		type_audio_player.play(0.09)
+		if character == "\n":
+			randomize()
+			var new_pitch_scale = typing_pitch_range.pick_random() / 10.0
+			type_audio_player.pitch_scale = new_pitch_scale
+		_print_character(character)
 		await get_tree().create_timer(0.0001).timeout
 	
+	_print_character("\n")
 	is_printing = false
 	printing_done.emit()
+
+func _print_character(character: String) -> void:
+	terminal_output.text += character
+	type_audio_player.play(0.09)
--- singletons/command_manager.gd
+++ singletons/command_manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 var commands: Dictionary
 
+var previous_command: String = ""
 
 func _init():
 	_restore_data()
@@ -10,13 +11,29 @@ func _init():
 func _restore_data():
 	commands = Globals.base_game_data["commands"]
 
-func has_command(command) -> bool:
+func has_command(command: String) -> bool:
 	return commands.has(command)
 
 func exec_command(command) -> String:
 	var command_dict = commands[command]
-	var _is_success = true
+	if previous_command == "exit":
+		if command == "yes":
+			get_tree().quit()
+		elif command == "no":
+			previous_command = ""
+			return ""
+		else:
+			return ""
 	if not command_dict.has_all(["success", "failure", "effects"]):
+		previous_command = command
 		return command_dict["success"][0]
-	return ""
+	
+	randomize()
+	var is_success = randi_range(0,10) <= 7
+	if is_success:
+		previous_command = command
+		return command_dict["success"].pick_random()
+	
+	previous_command = command
+	return command_dict["failure"].pick_random()
 	
